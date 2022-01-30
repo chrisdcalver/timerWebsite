95abb4bfcbbefefcf50698ed34716a41de7db0fc
chrisdcalver <chrisdcalver@gmail.com>
2022-01-30 21:58:25 +0000

    Screen render working

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,3 @@
-// Import other functions
-//import {countdownTimer} from '../JS/timerFunction'
-
 // Grab elements from the DOM
 const chairInput = document.getElementById("chair-input");
 const minuteInput = document.getElementById("minute-input");
@@ -9,31 +6,36 @@ const mainDisplay = document.getElementById("mainDisplay")
 
 // Global variables
 let chairNum = 0
-let newChairsArr = []
 
-// Button Pressed 
-submitBtn.addEventListener("click", function(e) {
+function renderDisplay() {
+    // Button Pressed 
+    submitBtn.addEventListener("click", function (e) {
 
-    //Stop page refreshing
-    e.preventDefault();
+        //Stop page refreshing
+        e.preventDefault();
 
-    // Assign values of input to new variables and turn to interger
-    const newChairInput = parseInt(chairInput.value);
-    const newMinuteInput = parseInt(minuteInput.value);
+        // Assign values of input to new variables and turn to interger
+        const newChairInput = parseInt(chairInput.value);
+        return newChairInput;
+        const newMinuteInput = parseInt(minuteInput.value);
+        return newMinuteInput;
 
-    // Create HTML div for chair display depending on user input
-    for (let i = 0; i < newChairInput.length; i++) {
+        // Create HTML div for chair display depending on user input
+        for (let i = 0; i < newChairInput; i++) {
 
-        chairNum += 1;
+            chairNum += 1;
 
-        const chairDiv = document.createElement("div");
-        chairDiv.id = "chair-area";
-        chairDiv.innerHTML += `
-            <h2 id="chairBanner">Chair: </h2>
-            <h4>Time Left: <span id="timer-display">15:00</span></h4>
+            const chairDiv = document.createElement("div");
+            chairDiv.classList.add("chairArea")
+            chairDiv.innerHTML += `
+            <h2 id="chairBanner">Chair: ${chairNum}</h2>
+            <h4>Time Left: <span id="timer-display">${newMinuteInput}:00</span></h4>
             <button id="startBtn">Start Timer</button>
             <button id="resetBtn">Reset Timer</button>
         `;
-        mainDisplay.appendChild("chairDiv")
-    };
-});
+            mainDisplay.appendChild(chairDiv);
+        };
+    });
+}
+
+renderDisplay()
