07a6e5d7a29472526982d6beaf23d5b9043338bd
chrisdcalver <chrisdcalver@gmail.com>
2022-02-04 21:50:17 +0000

    Still trying to get the timer to work

--- JS/main.js
+++ JS/main.js
@@ -2,7 +2,7 @@
 const chairInput = document.getElementById("chair-input");
 const minuteInput = document.getElementById("minute-input");
 const submitBtn = document.getElementById("input-submit");
-const mainDisplay = document.getElementById("mainDisplay")
+const mainDisplay = document.getElementById("mainDisplay");
 
 // Global variables
 let chairNum = 0
@@ -13,35 +13,29 @@ let chairArr = []
 
 function renderDisplay() {
 
-    // Button Pressed 
-    submitBtn.addEventListener("click", function (e) {
+    // Assign values of input to new variables and turn to interger
+    const newChairInput = parseInt(chairInput.value);
+    const newMinuteInput = parseInt(minuteInput.value);
 
-        //Stop page refreshing
-        e.preventDefault();
+    // Create HTML div for chair display depending on user input
+    for (let i = 0; i < newChairInput; i++) {
 
-        // Assign values of input to new variables and turn to interger
-        const newChairInput = parseInt(chairInput.value);
-        const newMinuteInput = parseInt(minuteInput.value);
+        chairNum += 1;
 
-        // Create HTML div for chair display depending on user input
-        for (let i = 0; i < newChairInput; i++) {
-
-            chairNum += 1;
-
-            const chairDiv = document.createElement("div");
-            chairDiv.classList.add("chairArea")
-            chairDiv.innerHTML += `
+        const chairDiv = document.createElement("div");
+        chairDiv.classList.add("chairArea")
+        chairDiv.innerHTML += `
                 <h2 id="chairBanner">Chair: ${chairNum}</h2>
                 <h4>Time Left: <span id="timer-display">${newMinuteInput}:00</span></h4>
                 <button id="startBtn">Start Timer</button>
                 <button id="resetBtn">Reset Timer</button>
         `;
-            mainDisplay.appendChild(chairDiv);
-        }; // for loop
+        mainDisplay.appendChild(chairDiv)
 
-    }); // submitBtn click
+    }; // for loop
 
 } // renderDisplay()
+
 
 function countdownTimer() {
 
@@ -82,8 +76,12 @@ function countdownTimer() {
     return [startBtnFn, resetBtnFn]
 
     // resetBtn.addEventListener('click', resetTimer)
-}
 
-renderDisplay()
+} // countdowntimer()
 
-countdownTimer()
+
+
+submitBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    renderDisplay()
+})
